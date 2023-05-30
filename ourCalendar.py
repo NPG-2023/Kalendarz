5c74009734354a1d37a4c4ceb180bbbcd24a56e4
Adam Maciuga <80308649+Mac-Adam@users.noreply.github.com>
2023-05-30 19:00:44 +0200

    Local storage (#5)
    * convert to and from fileFormat
    
    * read and load from file

--- ourCalendar.py
+++ ourCalendar.py
@@ -63,3 +63,26 @@ class Calendar:
                 if activity.endDate > start and activity.startDate < end:
                     res.append(activity)
         return res
+
+    def toFileFormat(self):
+        contents = f"{self.name}"
+        for activity in self.activities:
+            contents += "\n" + activity.toFileFormat()
+        return contents
+
+    @staticmethod
+    def fromFileFormat(contents):
+        data = contents.split("\n")
+        cal = Calendar(data[0])
+        for actStr in data[1:]:
+            cal.addActivity(Activity.fromFileFormat(actStr))
+        return cal
+
+    def saveToFile(self, path):
+        with open(path, 'w') as file:
+            file.write(self.toFileFormat())
+
+    @staticmethod
+    def loadFromFile(path):
+        with open(path, 'r') as file:
+            return Calendar.fromFileFormat(file.read())
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 import datetime
 from activity import Activity
 from ourCalendar import Calendar
+import os
 
 
 class TestActivity(unittest.TestCase):
@@ -39,6 +40,30 @@ class TestActivity(unittest.TestCase):
             str(activity),
             "test starts At 2023-04-03 18:00:00 and ends at 2023-04-03 19:00:00"
         )
+
+    def testToFileFormat(self):
+
+        date1 = datetime.datetime(2023, 4, 3, 18)
+        date2 = datetime.datetime(2023, 4, 3, 19)
+        activity = Activity("test", date2, date1)
+
+        self.assertEqual(
+            activity.toFileFormat(),
+            "test 1680537600.0 1680541200.0"
+        )
+
+    def testFromFileFormat(self):
+        with self.assertRaises(ValueError):
+            Activity.fromFileFormat("NotAValidFormat")
+
+        activity = Activity.fromFileFormat(
+            "test 1680537600.0 1680541200.0")
+        date1 = datetime.datetime(2023, 4, 3, 18)
+        date2 = datetime.datetime(2023, 4, 3, 19)
+
+        self.assertEqual(activity.name, "test")
+        self.assertEqual(activity.startDate, date1)
+        self.assertEqual(activity.endDate, date2)
 
 
 class TestCalendar(unittest.TestCase):
@@ -90,6 +115,47 @@ class TestCalendar(unittest.TestCase):
         )
         self.assertEqual(len(nonInclusive), 1)
         self.assertEqual(nonInclusive[0], activity2)
+
+    def testToFileFormat(self):
+
+        cal = Calendar('calendar')
+
+        date1 = datetime.datetime(2023, 4, 3, 18)
+        date2 = datetime.datetime(2023, 4, 3, 19)
+        cal.addActivity(Activity("test", date2, date1))
+
+        self.assertEqual(
+            cal.toFileFormat(),
+            "calendar"
+            "\ntest 1680537600.0 1680541200.0"""
+        )
+
+    def testFromFileFormat(self):
+
+        cal = Calendar.fromFileFormat(
+            "calendar"
+            "\ntest 1680537600.0 1680541200.0"""
+        )
+
+        self.assertEqual(cal.name, "calendar")
+        self.assertEqual(len(cal.activities), 1)
+        self.assertEqual(cal.activities[0].name, "test")
+
+    def testSaveAndLoadFromFile(self):
+        cal = Calendar("calendar")
+        date1 = datetime.datetime(2023, 4, 3, 18)
+        date2 = datetime.datetime(2023, 4, 3, 19)
+        activity = Activity("activity", date1, date2)
+        cal.addActivity(activity)
+
+        cal.saveToFile("./testCalendarSaveAndLoadFromFile.txt")
+        cal2 = Calendar.loadFromFile("./testCalendarSaveAndLoadFromFile.txt")
+
+        self.assertEqual(cal.name, cal2.name)
+        self.assertEqual(len(cal.activities), len(cal2.activities))
+        self.assertEqual(cal.activities[0].name, cal2.activities[0].name)
+
+        os.remove("./testCalendarSaveAndLoadFromFile.txt")
 
 
 if __name__ == "__main__":
