bf1b21da104b91853549ade93d43e7b7e7e0e70f
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2023-06-15 18:32:16 +0200

    Revert "Revert "automatic_save""
    This reverts commit 6d0636b172c5c9ea63b0a6aadea64510deabc31a.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from tkinter import Tk
 from gui import CalendarGUI
+from settings import Settings
+from ourCalendar import Calendar
 
 
 def main():
+    settings = Settings()
+    calendar = Calendar(settings.calendar_path)
     root = Tk()
     my_gui = CalendarGUI(root)
     root.mainloop()
