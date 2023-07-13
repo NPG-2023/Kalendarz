38307d9fb88a5068efc9bb9942b6c013812c4ccd
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2023-07-13 11:36:46 +0200

    dokumentacja

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     settings = Settings()
     calendar = Calendar(settings.calendar_path)
     root = Tk()
+    root.title("Kalendarz")
     my_gui = CalendarGUI(root, calendar, settings)
     root.mainloop()
 
